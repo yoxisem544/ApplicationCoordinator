e374c0eb04f9d7faa58a15ebb6f42df197bf4291
David <yoxisem544@gmail.com>
2018-06-26 20:39:23 +0800

    - add doc to view extension - add doc to app delegate

--- ApplicationCoordinator/Application/AppDelegate.swift
+++ ApplicationCoordinator/Application/AppDelegate.swift
@@ -2,28 +2,42 @@
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
   var window: UIWindow?
+  
   var rootController: UINavigationController {
+    // if you use storyboard, window exists
+    // when generate with code, remember to init new window
     return self.window!.rootViewController as! UINavigationController
   }
-  
+
+  // Whole app start here, by making a coordinator.
   private lazy var applicationCoordinator: Coordinator = self.makeCoordinator()
   
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+    // if app launch with remote notification
     let notification = launchOptions?[.remoteNotification] as? [String: AnyObject]
+    // build a deeplink option
     let deepLink = DeepLinkOption.build(with: notification)
+    // start application coordinator starts with option
     applicationCoordinator.start(with: deepLink)
+
     return true
   }
-  
+
   private func makeCoordinator() -> Coordinator {
-      return ApplicationCoordinator(
-        router: RouterImp(rootController: self.rootController),
-        coordinatorFactory: CoordinatorFactoryImp()
-      )
+    // make app coordinator.
+    return ApplicationCoordinator(
+      // Init a Router, injectable for testing here
+      // router need a nav controller as container
+      // FIXME: maybe a root window instead of nav controller
+      router: RouterImp(rootController: self.rootController),
+      // Init a coordinator factory, injectable here
+      coordinatorFactory: CoordinatorFactoryImp()
+    )
   }
   
-  //MARK: Handle push notifications and deep links
+  // MARK: - Handle push notifications and deep links
   func application(_ application: UIApplication,
                    didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
     let dict = userInfo as? [String: AnyObject]
--- ApplicationCoordinator/Extensions/UIViewExtensions.swift
+++ ApplicationCoordinator/Extensions/UIViewExtensions.swift
@@ -3,11 +3,13 @@ extension UIView {
   private class func viewInNibNamed<T: UIView>(_ nibNamed: String) -> T {
     return Bundle.main.loadNibNamed(nibNamed, owner: nil, options: nil)!.first as! T
   }
-  
+
+  /// Can be use as `UIView.nib()`
   class func nib() -> Self {
-    return viewInNibNamed(nameOfClass)
+    return viewInNibNamed(nameOfClass) // cause return type is Self, imply that T is Self.
   }
-  
+
+  /// Set nib's frame while init
   class func nib(_ frame: CGRect) -> Self {
     let view = nib()
     view.frame = frame
