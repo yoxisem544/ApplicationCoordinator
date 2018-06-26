5eefa02a685b3a6233e06f148a6338807efd6739
David <yoxisem544@gmail.com>
2018-06-26 18:02:32 +0800

    - add doc

--- ApplicationCoordinator/Protocols/Presentable.swift
+++ ApplicationCoordinator/Protocols/Presentable.swift
@@ -1,10 +1,14 @@
+// Presentable 可用來讓 Coordinator 知道這是可以導向的 object
 protocol Presentable {
   func toPresent() -> UIViewController?
 }
 
+// 原則上我們讓所有 UIViewController 都是 Presentable
 extension UIViewController: Presentable {
-  
+
   func toPresent() -> UIViewController? {
+    // 準備要 present 時回傳本身
     return self
   }
+
 }
