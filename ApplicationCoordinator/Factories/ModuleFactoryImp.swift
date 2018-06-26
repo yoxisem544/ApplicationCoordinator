927507b0d99fee26f52e48a0e46b00c62d209b48
David <yoxisem544@gmail.com>
2018-06-26 21:23:22 +0800

    - refactor module factory imp for readability

--- ApplicationCoordinator/Factories/ModuleFactoryImp.swift
+++ ApplicationCoordinator/Factories/ModuleFactoryImp.swift
@@ -1,42 +1,56 @@
-final class ModuleFactoryImp:
-  AuthModuleFactory,
-  OnboardingModuleFactory,
-  ItemModuleFactory,
-  ItemCreateModuleFactory,
-  SettingsModuleFactory {
-  
+final class ModuleFactoryImp { }
+
+extension ModuleFactoryImp : AuthModuleFactory {
+
   func makeLoginOutput() -> LoginView {
     return LoginController.controllerFromStoryboard(.auth)
   }
-  
+
   func makeSignUpHandler() -> SignUpView {
     return SignUpController.controllerFromStoryboard(.auth)
   }
-  
-  func makeOnboardingModule() -> OnboardingView {
-    return OnboardingController.controllerFromStoryboard(.onboarding)
-  }
-  
+
   func makeTermsOutput() -> TermsView {
     return TermsController.controllerFromStoryboard(.auth)
   }
-  
+
+}
+
+extension ModuleFactoryImp : OnboardingModuleFactory {
+
+  func makeOnboardingModule() -> OnboardingView {
+    return OnboardingController.controllerFromStoryboard(.onboarding)
+  }
+
+}
+
+extension ModuleFactoryImp : ItemModuleFactory {
+
   func makeItemsOutput() -> ItemsListView {
     return ItemsListController.controllerFromStoryboard(.items)
   }
-  
+
   func makeItemDetailOutput(item: ItemList) -> ItemDetailView {
-    
+
     let controller = ItemDetailController.controllerFromStoryboard(.items)
     controller.itemList = item
     return controller
   }
-  
+
+}
+
+extension ModuleFactoryImp : ItemCreateModuleFactory {
+
   func makeItemAddOutput() -> ItemCreateView {
     return ItemCreateController.controllerFromStoryboard(.create)
   }
-  
+
+}
+
+extension ModuleFactoryImp : SettingsModuleFactory {
+
   func makeSettingsOutput() -> SettingsView {
     return SettingsController.controllerFromStoryboard(.settings)
   }
+
 }
